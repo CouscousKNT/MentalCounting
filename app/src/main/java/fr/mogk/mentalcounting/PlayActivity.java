85514524184097fa838ec17b42b22cdc40622f92
BuildTools <unconfigured@null.spigotmc.org>
2022-02-06 15:14:53 +0100

    v1 design + ajout page score

--- app/src/main/java/fr/mogk/mentalcounting/PlayActivity.java
+++ app/src/main/java/fr/mogk/mentalcounting/PlayActivity.java
@@ -1,8 +1,14 @@
 package fr.mogk.mentalcounting;
 
+import android.app.ActionBar;
+import android.content.Intent;
 import android.view.Menu;
 import android.view.MenuInflater;
-import android.widget.TextView;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toolbar;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -13,7 +19,16 @@ public class PlayActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_play);
         //TextView tv = findViewById(R.id.msg_txt);
-        //tv.setTextColor(getResources().getColor(R.color.succes)); // test changement couleu, sera utile pour plus tard
+        //tv.setTextColor(getResources().getColor(R.color.succes)); // test changement couleur, sera utile pour plus tard
+        Button btn = findViewById(R.id.PlayToMenu);
+        btn.setText(btn.getText()+" →");
+        btn.setOnClickListener(view -> backToMenu(view));
+        setTitle(R.string.button_play);
+    }
+
+    public void backToMenu(View view){
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
     }
 
     @Override
@@ -21,5 +36,19 @@ public class PlayActivity extends AppCompatActivity {
         MenuInflater menuInflater = getMenuInflater();
         menuInflater.inflate(R.menu.main_menu, menu);
         return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.menu_scores:
+                Intent intent = new Intent(this, ScoreActivity.class);
+                startActivity(intent);
+            case R.id.menu_scores_reset:
+                //code pour reset les scores
+            default:
+                //rien
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
--- app/src/main/java/fr/mogk/mentalcounting/ScoreActivity.java
+++ app/src/main/java/fr/mogk/mentalcounting/ScoreActivity.java
@@ -1,5 +1,8 @@
 package fr.mogk.mentalcounting;
 
+import android.content.Intent;
+import android.view.View;
+import android.widget.Button;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +12,16 @@ public class ScoreActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_score);
+        Button btn = findViewById(R.id.ScoreToMenu);
+        btn.setText("← "+btn.getText());
+        btn.setOnClickListener(view -> backToMenu(view));
+        setTitle(R.string.button_score);
     }
+
+    public void backToMenu(View view){
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+    }
+
+
 }
